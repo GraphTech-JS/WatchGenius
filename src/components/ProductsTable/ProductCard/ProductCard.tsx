007272b3e2135de4cd072057eeef045f137bbc77
Danyl <kotvasilij1@gmail.com>
2025-10-21 15:59:33 +0300

    fix: icon + cards

--- src/components/ProductsTable/ProductCard/ProductCard.tsx
+++ src/components/ProductsTable/ProductCard/ProductCard.tsx
@@ -4,14 +4,15 @@ import Image from "next/image";
 import { LineChart } from "@/components/Main/Hero/Chart/LineChart";
 import styles from "./ProductCard.module.css";
 import { IWatch } from "@/interfaces";
-import { HeartIcon, BellIcon } from "../../../../public/social/Icon";
+import { BellIcon } from "../../../../public/social/Icon";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 const ArrowUp = () => (
   <svg width="16" height="14" viewBox="0 0 16 14" fill="none">
     <path
       d="
-      M8 0 V13
-      M8 1 L13 7
+      M8 1 V14
+      M8 2 L13 7
       M8 1 L3 7
       "
       stroke="currentColor"
@@ -65,6 +66,7 @@ export const ProductCard: React.FC<IWatch> = ({
     ArrowIcon = ArrowDown;
   }
 
+  const [isFavorite, setIsFavorite] = useState(false);
   const [chartHeight, setChartHeight] = useState(70);
   useEffect(() => {
     const updateHeight = () => {
@@ -81,17 +83,20 @@ export const ProductCard: React.FC<IWatch> = ({
 
   return (
     <div
-      className={`${styles.productCard} flex flex-col rounded-2xl h-[15rem] md:h-[21rem] px-2 md:px-4 py-3 md:py-3 max-w-[30rem] justify-between`}
+      className={`${styles.productCard} flex flex-col rounded-2xl h-[15rem] md:h-[21rem] px-3 md:px-4 py-2 md:py-3 max-w-[30rem] justify-between`}
     >
       {/* header */}
       <div className="relative mb-1 md:mb-4 flex w-full justify-center items-center">
-        <div
+        <button
+          onClick={() => setIsFavorite((prev) => !prev)}
           className={`${styles.productCardScore} absolute top-0 left-0 flex items-center justify-center w-8 h-8`}
         >
-          <HeartIcon
-            className={`${styles.productCardHeart} w-[20px] h-[18px] cursor-pointer`}
-          />
-        </div>
+          {isFavorite ? (
+            <FaHeart className="text-[#04694f] w-[20px] h-[18px] cursor-pointer transition-colors duration-300" />
+          ) : (
+            <FaRegHeart className="text-[#000000] w-[20px] h-[18px] cursor-pointer transition-colors duration-300" />
+          )}
+        </button>
 
         <Image
           src={image}
@@ -102,17 +107,17 @@ export const ProductCard: React.FC<IWatch> = ({
         />
 
         <div
-          className={`${styles.productCardScore} ${scoreClass} absolute top-0 right-0 flex items-center justify-center w-8 h-8 rounded-md font-bold`}
+          className={`${styles.productCardScore} ${scoreClass} absolute top-0 right-0 flex items-center justify-center w-6 md:w-8 h-6 md:h-8 rounded-md font-bold`}
         >
           {score}
         </div>
       </div>
 
       {/* body */}
-      <div className=" mb-0 md:mb-1 flex gap-2 items-center">
+      <div className=" mb-0 md:mb-1 flex gap-2 items-center justify-center">
         <div className="flex flex-col gap-0 md:gap-2">
           <div
-            className={`${styles.cardWatchName} text-center max-h-[54px] font-bold`}
+            className={`${styles.cardWatchName} text-center max-h-[54px] overflow-hidden font-bold`}
           >
             {brand}
           </div>
@@ -127,16 +132,16 @@ export const ProductCard: React.FC<IWatch> = ({
 
       {/* footer */}
       <div className="flex gap-2 items-center w-full">
-        <div className="relative text-center max-h-[54px] w-full flex flex-row justify-between">
+        <div className="relative text-center max-h-[54px] w-full flex flex-row justify-between items-center">
           <div className={`${styles.Price}`}>{price} €</div>
           <div
-            className={`${styles.marketCardHeadPercent} ${percentClass} absolute bottom-3 left-16 flex items-center justify-center gap-1 min-w-14  font-bold`}
+            className={`${styles.marketCardHeadPercent} ${percentClass} absolute bottom-3 left-14 flex items-center justify-center gap-0 min-w-14 font-bold`}
           >
             {ArrowIcon && <ArrowIcon />}
             {changePercent} %
           </div>
           <BellIcon
-            className={`${styles.productCardBell} w-[21px] h-[22px] cursor-pointer`}
+            className={`${styles.productCardBell} w-[13px] md:w-[21px] h-[16px] md:h-[22px] cursor-pointer`}
           />
         </div>
       </div>
