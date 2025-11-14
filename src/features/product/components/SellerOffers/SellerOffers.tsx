7cd23e71f74d19b6f4a33c3bf3dcaea353fd0811
Hellstaff8822 <fgjd.gh50@gmail.com>
2025-11-14 16:19:06 +0200

    minor-fix

--- src/features/product/components/SellerOffers/SellerOffers.tsx
+++ src/features/product/components/SellerOffers/SellerOffers.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import React, { useState, useEffect, useCallback } from "react";
-import Image from "next/image";
-import { ChevronDown } from "lucide-react";
-import { SellerOffersProps } from "@/interfaces/product";
+import React, { useState, useEffect, useCallback } from 'react';
+import Image from 'next/image';
+import { ChevronDown } from 'lucide-react';
+import { SellerOffersProps } from '@/interfaces/product';
 import {
   SafetyIcon,
   StarFillIcon,
   BxMapIcon,
   QuillLinkOutIcon,
-} from "@/product-icons";
-import styles from "./SellerOffers.module.css";
-import { t } from "@/i18n";
-import { productKeys } from "@/i18n/keys/product";
+} from '@/product-icons';
+import styles from './SellerOffers.module.css';
+import { t } from '@/i18n';
+import { productKeys } from '@/i18n/keys/product';
 
 const SellerOffers: React.FC<SellerOffersProps> = ({
   offers,
@@ -21,9 +21,9 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
   onConditionChange,
   onPurchase,
 }) => {
-  const [sortBy, setSortBy] = useState("price");
-  const [region, setRegion] = useState("all");
-  const [condition, setCondition] = useState("all");
+  const [sortBy, setSortBy] = useState('price');
+  const [region, setRegion] = useState('all');
+  const [condition, setCondition] = useState('all');
   const [openDropdown, setOpenDropdown] = useState<string | null>(null);
   const [filteredOffers, setFilteredOffers] = useState(offers);
 
@@ -45,37 +45,37 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
   const filterAndSortOffers = useCallback(() => {
     let filtered = [...offers];
 
-    if (region !== "all") {
+    if (region !== 'all') {
       filtered = filtered.filter((offer) => {
         const offerRegion = offer.location.toLowerCase();
         switch (region) {
-          case "europe":
+          case 'europe':
             return (
-              offerRegion.includes("європа") ||
-              offerRegion.includes("europe") ||
-              offerRegion.includes("німеччина") ||
-              offerRegion.includes("франція") ||
-              offerRegion.includes("італія") ||
-              offerRegion.includes("швейцарія") ||
-              offerRegion.includes("мюнхен") ||
-              offerRegion.includes("париж") ||
-              offerRegion.includes("цюрих")
+              offerRegion.includes('європа') ||
+              offerRegion.includes('europe') ||
+              offerRegion.includes('німеччина') ||
+              offerRegion.includes('франція') ||
+              offerRegion.includes('італія') ||
+              offerRegion.includes('швейцарія') ||
+              offerRegion.includes('мюнхен') ||
+              offerRegion.includes('париж') ||
+              offerRegion.includes('цюрих')
             );
-          case "usa":
+          case 'usa':
             return (
-              offerRegion.includes("сша") ||
-              offerRegion.includes("usa") ||
-              offerRegion.includes("америка") ||
-              offerRegion.includes("атланта") ||
-              offerRegion.includes("нью-йорк")
+              offerRegion.includes('сша') ||
+              offerRegion.includes('usa') ||
+              offerRegion.includes('америка') ||
+              offerRegion.includes('атланта') ||
+              offerRegion.includes('нью-йорк')
             );
-          case "asia":
+          case 'asia':
             return (
-              offerRegion.includes("азія") ||
-              offerRegion.includes("asia") ||
-              offerRegion.includes("японія") ||
-              offerRegion.includes("сингапур") ||
-              offerRegion.includes("токіо")
+              offerRegion.includes('азія') ||
+              offerRegion.includes('asia') ||
+              offerRegion.includes('японія') ||
+              offerRegion.includes('сингапур') ||
+              offerRegion.includes('токіо')
             );
           default:
             return true;
@@ -83,28 +83,28 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
       });
     }
 
-    if (condition !== "all") {
+    if (condition !== 'all') {
       filtered = filtered.filter((offer) => {
         const offerCondition = offer.details.toLowerCase();
         switch (condition) {
-          case "new":
+          case 'new':
             return (
-              offerCondition.includes("новий") ||
-              offerCondition.includes("new") ||
-              offerCondition.includes("гарантією") ||
-              offerCondition.includes("сертифікатом") ||
-              offerCondition.includes("документами")
+              offerCondition.includes('новий') ||
+              offerCondition.includes('new') ||
+              offerCondition.includes('гарантією') ||
+              offerCondition.includes('сертифікатом') ||
+              offerCondition.includes('документами')
             );
-          case "used":
+          case 'used':
             return (
-              offerCondition.includes("відмінний") ||
-              offerCondition.includes("excellent")
+              offerCondition.includes('відмінний') ||
+              offerCondition.includes('excellent')
             );
-          case "refurbished":
+          case 'refurbished':
             return (
-              offerCondition.includes("б/у") ||
-              offerCondition.includes("used") ||
-              offerCondition.includes("відновлений")
+              offerCondition.includes('б/у') ||
+              offerCondition.includes('used') ||
+              offerCondition.includes('відновлений')
             );
           default:
             return true;
@@ -113,17 +113,17 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
     }
 
     filtered.sort((a, b) => {
-      const priceA = parseFloat(a.price.replace(/[^\d]/g, ""));
-      const priceB = parseFloat(b.price.replace(/[^\d]/g, ""));
+      const priceA = parseFloat(a.price.replace(/[^\d]/g, ''));
+      const priceB = parseFloat(b.price.replace(/[^\d]/g, ''));
 
       switch (sortBy) {
-        case "price":
+        case 'price':
           return priceA - priceB;
-        case "price-desc":
+        case 'price-desc':
           return priceB - priceA;
-        case "rating":
+        case 'rating':
           return b.rating - a.rating;
-        case "reviews":
+        case 'reviews':
           return b.reviewsCount - a.reviewsCount;
         default:
           return 0;
@@ -143,14 +143,14 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (!(event.target as Element).closest(".filterDropdown")) {
+      if (!(event.target as Element).closest('.filterDropdown')) {
         closeDropdown();
       }
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
 
@@ -170,9 +170,9 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
     return (
       <div className={`filterDropdown ${styles.filterDropdown}`}>
         <button
-          type="button"
+          type='button'
           className={`${styles.filterDropdownButton} ${
-            isOpen ? styles.open : ""
+            isOpen ? styles.open : ''
           }`}
           onClick={() => toggleDropdown(name)}
         >
@@ -182,7 +182,7 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
           <ChevronDown
             size={26}
             className={`absolute right-[20px] top-1/2 -translate-y-1/2 transition-all duration-300 ease-in-out ${
-              isOpen ? "rotate-180 text-black" : "text-[#8b8b8b]"
+              isOpen ? 'rotate-180 text-black' : 'text-[#8b8b8b]'
             }`}
           />
         </button>
@@ -195,7 +195,7 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
           {options.map((option) => (
             <button
               key={option.value}
-              type="button"
+              type='button'
               className={styles.filterDropdownOption}
               onClick={() => {
                 onChange(option.value);
@@ -211,8 +211,8 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
   };
 
   return (
-    <div className="bg-white rounded-lg">
-      <div className="mb-6">
+    <div className='overflow-hidden max-w-full bg-white rounded-lg'>
+      <div className='mb-6'>
         <h3
           className={`${styles.titleFont} mb-2 font-semibold text-gray-900 text-[20px] xl:text-[36px]`}
         >
@@ -222,90 +222,90 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
         </h3>
       </div>
 
-      <div className="flex flex-col sm:flex-row items-center sm:w-full sm:items-center mb-6 gap-[12px] xl:gap-[35px] w-full mx-auto">
-        <div className="flex flex-row justify-between sm:flex-row items-center sm:items-center gap-2 xl:gap-[26px] w-full sm:w-auto">
+      <div className='flex flex-col sm:flex-row items-center sm:items-center mb-6 gap-[5px] xl:gap-[35px] w-full max-w-full flex-wrap'>
+        <div className='flex flex-row justify-between sm:flex-row items-center sm:items-center gap-2 xl:gap-[26px] w-full sm:w-auto min-w-0'>
           <label
             className={`${styles.labelFont} text-base font-medium text-gray-500 text-[16px] sm:text-[15px] xl:text-[20px]`}
           >
             {t(productKeys.offers.sort)}:
           </label>
           <CustomDropdown
-            name="sort"
+            name='sort'
             value={sortBy}
             onChange={handleSortChange}
             options={[
               {
-                value: "price",
+                value: 'price',
                 label: t(productKeys.offers.sortOptions.priceAsc),
                 displayLabel: t(productKeys.offers.sortOptions.priceBy),
               },
               {
-                value: "price-desc",
+                value: 'price-desc',
                 label: t(productKeys.offers.sortOptions.priceDesc),
                 displayLabel: t(productKeys.offers.sortOptions.priceBy),
               },
               {
-                value: "rating",
+                value: 'rating',
                 label: t(productKeys.offers.sortOptions.rating),
               },
               {
-                value: "reviews",
+                value: 'reviews',
                 label: t(productKeys.offers.sortOptions.reviews),
               },
             ]}
           />
         </div>
 
-        <div className="flex flex-row justify-between  sm:flex-row items-center sm:items-center gap-2 xl:gap-[26px] w-full sm:w-auto">
+        <div className='flex flex-row justify-between sm:flex-row items-center sm:items-center gap-2 xl:gap-[26px] w-full sm:w-auto min-w-0'>
           <label
             className={`${styles.labelFont} text-base font-medium text-gray-500 text-[16px] xl:text-[20px]`}
           >
             {t(productKeys.offers.region)}:
           </label>
           <CustomDropdown
-            name="region"
+            name='region'
             value={region}
             onChange={handleRegionChange}
             options={[
-              { value: "all", label: t(productKeys.offers.regionOptions.all) },
+              { value: 'all', label: t(productKeys.offers.regionOptions.all) },
               {
-                value: "europe",
+                value: 'europe',
                 label: t(productKeys.offers.regionOptions.europe),
               },
-              { value: "usa", label: t(productKeys.offers.regionOptions.usa) },
+              { value: 'usa', label: t(productKeys.offers.regionOptions.usa) },
               {
-                value: "asia",
+                value: 'asia',
                 label: t(productKeys.offers.regionOptions.asia),
               },
             ]}
           />
         </div>
 
-        <div className="flex flex-row justify-between  sm:flex-row items-center sm:items-center gap-2 xl:gap-[26px] w-full sm:w-auto">
+        <div className='flex flex-row justify-between sm:flex-row items-center sm:items-center gap-2 xl:gap-[26px] w-full sm:w-auto min-w-0'>
           <label
             className={`${styles.labelFont} text-base font-medium text-gray-500 xl:text-[20px]`}
           >
             {t(productKeys.offers.condition)}:
           </label>
           <CustomDropdown
-            name="condition"
+            name='condition'
             value={condition}
             onChange={handleConditionChange}
             options={[
               {
-                value: "all",
+                value: 'all',
                 label: t(productKeys.offers.conditionOptions.all),
               },
               {
-                value: "new",
+                value: 'new',
                 label: t(productKeys.offers.conditionOptions.new),
               },
               {
-                value: "used",
+                value: 'used',
                 label: t(productKeys.offers.conditionOptions.used),
               },
               {
-                value: "refurbished",
+                value: 'refurbished',
                 label: t(productKeys.offers.conditionOptions.refurbished),
               },
             ]}
@@ -316,10 +316,10 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
       <div className={styles.safetyBlock}>
         <Image
           src={SafetyIcon}
-          alt="Safety"
+          alt='Safety'
           width={33}
           height={33}
-          className="mr-3"
+          className='mr-3'
         />
         <p
           className={`${styles.textFont} text-sm font-normal text-gray-600 sm:text-[15px] lg:text-[16px]`}
@@ -329,7 +329,7 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
       </div>
 
       {filteredOffers.filter((offer) => offer.isSecure).length > 0 ? (
-        <div className="grid grid-cols-1 gap-[18px] xl:gap-[21px] md:grid-cols-2">
+        <div className='grid grid-cols-1 gap-[18px] xl:gap-[21px] md:grid-cols-2'>
           {filteredOffers.map((offer) => (
             <div key={offer.id} className={styles.sellerCard}>
               <div className={styles.sellerCardContent}>
@@ -342,18 +342,18 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
                   {offer.isSecure && (
                     <div
                       style={{
-                        borderRadius: "15px",
-                        width: "36px",
-                        height: "34px",
-                        backgroundColor: "#dceae6",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
+                        borderRadius: '15px',
+                        width: '36px',
+                        height: '34px',
+                        backgroundColor: '#dceae6',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
                       }}
                     >
                       <Image
                         src={SafetyIcon}
-                        alt="Safety"
+                        alt='Safety'
                         width={18}
                         height={18}
                       />
@@ -362,17 +362,17 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
                   <div
                     className={styles.sellerCardPriceDesktop}
                     style={{
-                      color: offer.isSecure ? "#4cba9e" : "#f25454",
+                      color: offer.isSecure ? '#4cba9e' : '#f25454',
                     }}
                   >
                     {offer.price} {offer.currency}
                   </div>
                 </div>
 
-                <div className="flex items-center">
+                <div className='flex items-center'>
                   <Image
                     src={StarFillIcon}
-                    alt="Star"
+                    alt='Star'
                     width={26}
                     height={26}
                     className={`lg:w-[26px] lg:h-[26px] w-[22px] h-[22px] ${styles.starIcon}`}
@@ -384,10 +384,10 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
                   </span>
                   <Image
                     src={BxMapIcon}
-                    alt="Location"
+                    alt='Location'
                     width={24}
                     height={24}
-                    style={{ marginRight: "8px" }}
+                    style={{ marginRight: '8px' }}
                   />
                   <span
                     className={`${styles.textFont} text-[15px] font-normal text-gray-600`}
@@ -412,7 +412,7 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
                   <div
                     className={styles.sellerCardPriceMobile}
                     style={{
-                      color: offer.isSecure ? "#4cba9e" : "#f25454",
+                      color: offer.isSecure ? '#4cba9e' : '#f25454',
                     }}
                   >
                     {offer.price} {offer.currency}
@@ -423,7 +423,7 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
                   >
                     <Image
                       src={QuillLinkOutIcon}
-                      alt="Link"
+                      alt='Link'
                       width={16}
                       height={16}
                     />
@@ -435,7 +435,7 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
                   >
                     <Image
                       src={QuillLinkOutIcon}
-                      alt="Link"
+                      alt='Link'
                       width={16}
                       height={16}
                     />
@@ -454,7 +454,7 @@ const SellerOffers: React.FC<SellerOffersProps> = ({
           <button className={styles.requestOfferButton}>
             <Image
               src={QuillLinkOutIcon}
-              alt="Request"
+              alt='Request'
               width={16}
               height={16}
             />
